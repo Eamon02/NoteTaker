0072a1fdc5aac181d84089c125d908847f74f0fe
Eamon McHugh <eamonmchugh2@gmail.com>
2021-02-04 22:06:24 -0500

    fixed heroku

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const noteList = require("./db/db.json");
 // Sets up the Express App
 // =============================================================
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // =============================================================
 // Sets up the Express app to handle data parsing
